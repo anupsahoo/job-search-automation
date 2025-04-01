2eb30a42797d8f0de6cc7ab3c8e53dd2d3e7e926
anup <11258608+anupsahoo@users.noreply.github.com>
2025-04-02 02:21:45 +0530

    Update config.py

--- LINKEDIN/config.py
+++ LINKEDIN/config.py
@@ -1,5 +1,7 @@
-JOB_TITLE = "Software Automation Engineer"
-LOCATIONS = ["Europe", "Australia", "UAE", "Singapore"]
-NUM_JOBS = 10
-USERNAME = "sahooanuptech@gmail.com"
-PASSWORD = "Sairam@2025"
+//  do not use your original LinkedIn account. Create a new one with a new email to get the job recommendations and apply with your original one 
+
+JOB_TITLE = "Software Automation Engineer" // mention the job that you want to apply 
+LOCATIONS = ["Europe", "Australia", "UAE", "Singapore"] // mention location 
+NUM_JOBS = 10 // mention number of jobs that you want to look for 
+USERNAME = "your email " // your email
+PASSWORD = "your password " // add your password
